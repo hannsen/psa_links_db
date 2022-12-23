29221765894e224db8694826b544c465c38266f8
jan.karberg <jan.karberg@quodata.de>
2022-12-23 15:11:29 +0100

    switch 2

--- tuya/switch.py
+++ tuya/switch.py
@@ -6,6 +6,11 @@ switches = {
     'device_id': 'bff8da7da6949373e6ivbd',
     'ip': '192.168.178.20',
     'local_key': 'ef9409c25efa2fd6',
+  },
+  'switch_2': {
+    'device_id': 'bf8de7a491ba5116efgtvy',
+    'ip': '192.168.178.35',
+    'local_key': 'd283a19fc27105f7',
   }
 }
 id = sys.argv[1]
